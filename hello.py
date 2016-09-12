7a4805a9c07b93c57533428f1cba43bd6bb8a86d
Younes <younes@interactive-object.com>
2016-09-12 17:12:14 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -20,7 +20,34 @@ SPEED_2V = MAX_SPEED / 2
 class AppSession(ApplicationSession):
 
     log = Logger()
+    _voice = "slt"
+    _flite = "/usr/bin/flite"
 
+    @inlineCallbacks
+    def say(self, text):
+        """
+        Speak text.
+        """
+        if self._is_busy:
+            raise Exception("already talking")
+        else:
+            # mark TTS engine as busy and publish event
+            self._is_busy = True
+            self.publish('com.interactive-object.iot.speechsynth.on_speech_start', text)
+
+            # start TTS
+            yield getProcessOutput(self._flite, ['-voice', self._voice, '-t', text])
+
+            # mark TTS engine as free and publish event
+            self._is_busy = False
+            self.publish('com.interactive-object.iot.speechsynth.on_speech_end')
+
+    def is_busy(self):
+        """
+        Check if TTS engine is currently busy speaking.
+        """
+        return self._is_busy
+        
     @inlineCallbacks
     def onJoin(self, details):
 
@@ -29,9 +56,22 @@ class AppSession(ApplicationSession):
         def onhello(msg):
             self.log.info("event for 'onhello' received: {msg}", msg=msg)
 
-        yield self.subscribe(onhello, 'com.interactive-object.onhello')
+        yield self.subscribe(onhello, 'com.interactive-object.iot.onhello')
         self.log.info("subscribed to topic 'onhello'")
 
+        # becomes true when currently speaking
+        self._is_busy = False
+
+        # register methods on this object for remote calling via WAMP
+        for proc in [self.say, self.is_busy]:
+            uri = u'com.interactive-object.iot.speechsynth.{}'.format(proc.__name__)
+            yield self.register(proc, uri)
+            log.msg("SpeechSynthAdapter registered procedure {}".format(uri))
+
+        # signal we are done with initializing our component
+        self.publish('com.interactive-object.iot.speechsynth.on_ready')
+        log.msg("SpeechSynthAdapter ready.")
+        
         # REGISTER a procedure for remote calling
         #
         def forwardOn():
@@ -68,15 +108,15 @@ class AppSession(ApplicationSession):
         def rightOff():
             motors.motor1.setSpeed(0)
             
-        yield self.register(forwardOn, 'com.interactive-object.forwardon')
-        yield self.register(forwardOff, 'com.interactive-object.forwardoff')
-        yield self.register(backwardOn, 'com.interactive-object.backwardon')
-        yield self.register(backwardOff, 'com.interactive-object.backwardoff')
+        yield self.register(forwardOn, 'com.interactive-object.iot.forwardon')
+        yield self.register(forwardOff, 'com.interactive-object.iot.forwardoff')
+        yield self.register(backwardOn, 'com.interactive-object.iot.backwardon')
+        yield self.register(backwardOff, 'com.interactive-object.iot.backwardoff')
 
-        yield self.register(leftOn, 'com.interactive-object.lefton')
-        yield self.register(leftOff, 'com.interactive-object.leftoff')
-        yield self.register(rightOn, 'com.interactive-object.righton')
-        yield self.register(rightOff, 'com.interactive-object.rightoff')
+        yield self.register(leftOn, 'com.interactive-object.iot.lefton')
+        yield self.register(leftOff, 'com.interactive-object.iot.leftoff')
+        yield self.register(rightOn, 'com.interactive-object.iot.righton')
+        yield self.register(rightOff, 'com.interactive-object.iot.rightoff')
         
         self.log.info("procedure registered")
 
